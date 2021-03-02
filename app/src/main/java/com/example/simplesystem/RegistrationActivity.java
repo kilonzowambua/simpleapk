201827dfe3c47589ef20af94c9dc150325a0c7c8
kilonzowambua <kilonzowambua254@gmail.com>
2021-03-02 11:13:14 +0300

    Updated

--- app/src/main/java/com/example/simplesystem/RegistrationActivity.java
+++ app/src/main/java/com/example/simplesystem/RegistrationActivity.java
@@ -6,7 +6,10 @@ import androidx.lifecycle.ViewModel;
 
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.simplesystem.data.LoginRepository;
 import com.example.simplesystem.data.Result;
@@ -15,10 +18,46 @@ import com.example.simplesystem.R;
 
 import static com.example.simplesystem.R.layout.activity_register;
 
-public class RegistrationActivity extends AppCompatActivity {
-    private EditText
-    public void onCreate(Bundle savedInstanceState) {
+public class RegistrationActivity<Botton> extends AppCompatActivity {
+    private EditText user_name, user_email ,user_password;
+    private Button register;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_register);
+        declarevarables();
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()){
+                    //upload to db
+                    
+                }
+
+            }
+        });
     }
+   private void declarevarables(){
+        user_name = (EditText)findViewById(R.id.user_name);
+        user_email = (EditText)findViewById(R.id.user_email);
+        user_password = (EditText)findViewById(R.id.user_password);
+        register = (Button)findViewById(R.id.register);
+    }
+    private  Boolean validate()
+    {
+        Boolean results = false;
+        String name = user_name.getText().toString();
+        String email = user_email.getText().toString();
+        String password = user_email.getText().toString();
+        if(name.isEmpty() && email.isEmpty() && password.isEmpty()) {
+            Toast.makeText(this, "Please Enter All Fields", Toast.LENGTH_SHORT).show();
+               }
+        else
+               {
+      results = true;
+               }
+        return results;
+    }
+
+
 }
